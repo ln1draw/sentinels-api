77e8fc8de0e5bf5ed23caf74def00abc4ca12b86
shanghaiellen <shanghaiellen@gmail.com>
2014-03-26 11:31:57 -0700

    initial commit for api

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SentinelsApi::Application.config.secret_key_base = '4d2e34067a6a2dd31a865fc504b8e23cfc03229d759e24e49f0538b278bd0081623c537823b605827bddf165724f546fa3307abd56c30bb5d48eea0b7feb70b2'
